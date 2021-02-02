a53f2b7c3e0bea02fd71acf277a56e9b82447172
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2021-02-01 17:37:57 -0700

    Update d3soundingchart.js

--- d3soundingchart.js
+++ d3soundingchart.js
@@ -1,33 +1,34 @@
 function draw_lapse_chart (data, maxTemp, dalr) {
-    let visibleScreenWidth = document.documentElement.clientWidth * 0.9;
-    let margin = {top: 15, right: visibleScreenWidth * 0.026, bottom: 80, left: visibleScreenWidth * 0.09};
-    let width = visibleScreenWidth - margin.left - margin.right;
-    let height = 660 - margin.top - margin.bottom;
-    let svg = d3.select('#skewt').append('svg')
+    const visibleScreenWidth = document.documentElement.clientWidth * 0.9;
+    const margin = {top: 15, right: visibleScreenWidth * 0.026, bottom: 80, left: visibleScreenWidth * 0.09};
+    const width = visibleScreenWidth - margin.left - margin.right;
+    const height = 660 - margin.top - margin.bottom;
+    const svg = d3.select('#skewt').append('svg')
         .attr('class', 'svg-bg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
         .append('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    let x = d3.scaleLinear()
+    const x = d3.scaleLinear()
         .range([0, width])
         .domain([0,100]);
-    let y = d3.scaleLinear()
+    const y = d3.scaleLinear()
         .range([height, 0])
         .domain([4,18]);
-    let c1 = x(25);
-    let c2 = x(100);
-    let c3 = y(4);
-    let polygon = 'M ' + c1 + ' 0, L ' + c2 + ' ' + c3 + ', L ' + (width + margin.right) + ' ' + c3 + ', L ' + (width + margin.right) + ' 0, L ' + c1 + ' 0';
-    let xAxisGrid = d3.axisTop(x)
+    const c1 = x(25);
+    const c2 = x(100);
+    const c3 = y(4);
+    const polygon = 'M ' + c1 + ' 0, L ' + c2 + ' ' + c3 + ', L ' + (width + margin.right) + ' ' + c3 + ', L ' + (width + margin.right) + ' 0, L ' + c1 + ' 0';
+    const dalrLabelPosition = 'rotate(43, 0, ' + visibleScreenWidth * 0.8 + ')';
+    const xAxisGrid = d3.axisTop(x)
         .tickSize(0-y(4))
         .tickFormat('')
         .ticks(19);
-    let dalrAxisGrid = d3.range(0,105,5);
-    let yAxisGrid = d3.axisLeft(y)
+    const dalrAxisGrid = d3.range(0,105,5);
+    const yAxisGrid = d3.axisLeft(y)
         .tickSize(-innerWidth)
         .tickFormat('')
-    let tempLine = d3.line()
+    const tempLine = d3.line()
         .x(function(d) { return x((d.Temp_c * 9 / 5) + 32); })
         .y(function(d) { return y(d.Altitude_m * 3.281 / 1000); });
     svg.append('g') // Draw vertical x axis gridlines
@@ -89,9 +90,10 @@ function draw_lapse_chart (data, maxTemp, dalr) {
         .attr('fill', 'rgb(80,80,80)');
     svg.append('text') // DALR label
         .attr('class', 'dalrLabel')
-        .attr('x', visibleScreenWidth * 0.47)
-        .attr('y', -160)
-        .attr('transform', 'rotate(43)')
+        // .attr('x', x(55))
+        // .attr('y', 200)
+        .attr('transform', dalrLabelPosition)
+        .style('text-anchor', 'start')
         .text('\u2190 DALR (-5.38 \u00B0F / 1000 ft) \u2192');
     svg.append('line') // Legend green line
         .attr('stroke', 'lightgreen')
