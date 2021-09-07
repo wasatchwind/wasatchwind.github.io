f4d2c744a285cf0d8740e9611459c810185ac75e
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2021-09-06 21:16:55 -0600

    Update gcp.js

--- gcp.js
+++ gcp.js
@@ -51,7 +51,6 @@
     const reportdate = new Date(soarData['Report date'])
     console.log(soarData)
     console.log('now: ', now)
-    console.log('utc test: ', now.UTC)
     console.log('report date: ', reportdate.toLocaleString('en-us', {weekday: 'short', month: 'short', day: 'numeric'}))
     console.log('now date: ', now.toLocaleString('en-us', {timezone: 'America/Denver', weekday: 'short', month: 'short', day: 'numeric'}))
     if (reportdate.toLocaleString('en-us', {weekday: 'short', month: 'short', day: 'numeric'}) === now.toLocaleString('en-us', {weekday: 'short', month: 'short', day: 'numeric'})) {
