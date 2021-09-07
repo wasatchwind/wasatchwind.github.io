06cc151948498ac8ffcb2f368f5f7b6e5087a8f6
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2021-09-07 17:59:37 -0600

    Update gcp.js

--- gcp.js
+++ gcp.js
@@ -48,6 +48,8 @@
     const response = await fetch(url)
     const soarData = await response.json()
     const odt = (soarData['Overdevelopment time'] === '0000') ? 'None' : soarData['Overdevelopment time']
+    console.log('report date: ', soarData['Report date'])
+    console.log('now date: ', now.toLocaleString('en-us', {month: '2-digit', day: '2-digit', year: 'numeric'}))
     if (soarData['Report date']===now.toLocaleString('en-us', {month: '2-digit', day: '2-digit', year: 'numeric'})) {
         console.log('match')
         const maxTemp = soarData['Max temp']
