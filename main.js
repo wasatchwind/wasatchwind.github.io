9fb95c954d9c650de13b0ee77681e7ae702f7bb1
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2021-09-10 22:22:32 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict';
 const date = new Date().toUTCString()
-console.log('raw utc date: ', date)
 const now = new Date(date)
-const test = new Date().toISOString()
-console.log('raw iso date: ', test)
+
+const test = new Date()
+console.log('date: ', test.getDate())
+console.log('month: ', test.getMonth())
+
 const headingDate = now.toLocaleString('en-us', {weekday: 'short', month: 'short', day: 'numeric'})
 document.getElementById('heading-date').innerHTML = headingDate
 const wwGrn = '#20c997' // bootstrap teal
