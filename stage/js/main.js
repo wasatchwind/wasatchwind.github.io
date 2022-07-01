00203dc50a6d0943f197e68e17665650ed5e8dd1
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-07-01 16:10:44 -0600

    Update main.js

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,1 +1,19 @@
-tbd
+'use strict';
+const now = new Date();
+let currentDiv = 'wind'
+document.getElementById(currentDiv).style.display = 'block'
+
+function reload() {
+    history.scrollRestoration = 'manual'
+    location.reload()
+};
+
+function toggleDiv(newDiv) {
+    document.getElementById(currentDiv).style.display = 'none'
+    document.getElementById(`${currentDiv}-title`).className = 'display-3 fw-bold text-warning'
+    document.getElementById(`${currentDiv}-border`).className = 'tile-border'
+    document.getElementById(`${newDiv}-title`).className = 'display-3 fw-bold'
+    document.getElementById(`${newDiv}-border`).className = 'tile-border-selected'
+    document.getElementById(newDiv).style.display = 'block'
+    currentDiv = newDiv
+};
