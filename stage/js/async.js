e48d32fb38e50ccb0a2446dd1cc58a02188f5bb2
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-07-03 17:08:57 -0600

    Update async.js

--- stage/js/async.js
+++ stage/js/async.js
@@ -1,2 +1,11 @@
-    document.getElementById('latest-icon').src = './/images/sct.png'
-    document.getElementById('latest-cam').src = './/images/latest-cam.jpg'
+    // LOCAL TESTING
+    const maxTempF = 102
+    document.getElementById('max-temp').innerHTML = `${maxTempF}&deg;`
+    document.getElementById('latest-icon').src = 'images/sct.png'
+    document.getElementById('latest-cam').src = 'images/latest-cam.jpg'
+    document.getElementById('latest-zone').innerHTML = '&#9471;'
+    document.getElementById('latest-zone').style.color = 'var(--bs-red)'
+
+
+    // REAL CODE
+    timeSeries(timeSeriesData)
