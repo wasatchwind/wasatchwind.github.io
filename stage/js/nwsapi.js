0febfe1ae0de5948759df04002cb601086f5df39
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-08-14 15:08:14 -0600

    Update nwsapi.js

--- stage/js/nwsapi.js
+++ stage/js/nwsapi.js
@@ -1,11 +1,8 @@
 'use strict'; // https://www.weather.gov/documentation/services-web-api
 function maxTemp(data) {
     const latest = data.properties.periods[0]
-    const maxTempF = latest.isDaytime ? `${latest.temperature}&deg;` : 'Check<br>Tomorrow'
-    const fontSize = Number.isFinite(parseInt(maxTempF)) ? 'display-4 fw-bold' : 'fs-4'
-    const element = document.getElementById('max-temp')
-    element.innerHTML = maxTempF
-    element.className = fontSize
+    const maxTempF = !latest.isDaytime ? `${latest.temperature}&deg;` : '--'
+    document.getElementById('max-temp').innerHTML = maxTempF
     return parseInt(maxTempF)
 };
 
