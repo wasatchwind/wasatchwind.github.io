88c265191aa5d98e633dcc8e2db9c8573cf73630
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-08-21 16:10:01 -0600

    Update main.js

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 const now = new Date();
 const titleDate = now.toLocaleString('en-us', {weekday: 'short', month: 'short', day: 'numeric'})
+document.getElementById('title-date').innerHTML = titleDate
 const nextDay = now.getHours() > 18 ? `&nbsp;&nbsp;(${new Date(now.setHours(now.getHours() + 24)).toLocaleString('en-us', {weekday: 'long'})})&nbsp;&nbsp;` : ''
 let currentDiv = 'wind'
 let liftParams = {}
