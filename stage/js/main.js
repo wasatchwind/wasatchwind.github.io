8a29f3f10a1f0db8e08938d0a5538cf99cfc57d8
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-09-27 13:00:40 -0600

    Update main.js

--- stage/js/main.js
+++ stage/js/main.js
@@ -3,6 +3,7 @@ const now = new Date();
 const titleDate = now.toLocaleString('en-us', {weekday: 'short', month: 'short', day: 'numeric'})
 document.getElementById('title-date').innerHTML = titleDate
 const nextDay = now.getHours() > 18 ? `&nbsp;&nbsp;(${new Date(now.setHours(now.getHours() + 24)).toLocaleString('en-us', {weekday: 'long'})})&nbsp;&nbsp;` : ''
+const screenWidth = document.documentElement.clientWidth
 let currentDiv = 'wind', liftParams = {}, maxTempF, soundingData = {}
 
 function reload() {
