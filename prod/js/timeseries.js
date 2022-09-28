10e61246e06760987fe11c98c8cc78ca4612e46c
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-09-28 15:10:58 -0600

    Update timeseries.js

--- prod/js/timeseries.js
+++ prod/js/timeseries.js
@@ -1,121 +1,156 @@
 'use strict';
-// IIFE ASYNC MESONET PRIVATE API FOR TIME SERIES: https://developers.synopticdata.com/mesonet
-(async () => {
-    const nwstokenUrl = 'https://storage.googleapis.com/wasatch-wind-static/nwstoken.json'
-    const nwstokenResponse = await fetch(nwstokenUrl)
-    const nwstoken = await nwstokenResponse.json()
-    const url = `https://api.mesowest.net/v2/station/timeseries?&stid=KSLC&stid=UTOLY&stid=AMB&stid=KU42&stid=FPS&stid=OGP&stid=HF012&recent=420&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=${nwstoken.token}`
-    const response = await fetch(url)
-    const tsData = await response.json()
-    if (tsData) {
-        if (tsData.STATION[0].STID==='KSLC') kslcAltiTempZone(tsData.STATION[0].OBSERVATIONS)
-        let stations = []
-        for (let i=0; i<tsData.STATION.length; i++) {
-            stations[i] = tsData.STATION[i].OBSERVATIONS
-            stations[i].stid = tsData.STATION[i].STID
-            windChart(stations[i])
+function ensureGustData(data) {
+    for (let i=0; i<data.STATION.length; i++) data.STATION[i].OBSERVATIONS.wind_gust_set_1 = data.STATION[i].OBSERVATIONS.wind_gust_set_1 ? data.STATION[i].OBSERVATIONS.wind_gust_set_1 : data.STATION[i].OBSERVATIONS.wind_gust_set_1 = new Array(12).fill(null)
+};
+
+function kslcTiles(data) {
+    windTileTimeRange(data.date_time.slice(-12))
+    windDirection('tile', data.wind_direction_set_1.slice(-12))
+    windBarHeight('tile', data.wind_speed_set_1.slice(-12), data.wind_gust_set_1.slice(-12))
+    windBarColor('tile', data.wind_speed_set_1.slice(-12))
+    windSpeed('tile', data.wind_speed_set_1.slice(-12))
+    windGust('tile', data.wind_gust_set_1.slice(-12))
+    zone(data.air_temp_set_1, data.altimeter_set_1, data.date_time)
+    document.getElementById('latest-temp').innerHTML = data.air_temp_set_1[data.air_temp_set_1.length - 1] ? `${Math.round(data.air_temp_set_1[data.air_temp_set_1.length - 1])}&deg;` : '--'
+};
+
+function windTileTimeRange(data) {
+    const start = data[0].toLowerCase()
+    const end = data[11].toLowerCase()
+    document.getElementById('wind-time-range').innerHTML = `${start} - ${end} @ KSLC`
+};
+
+function windDirection(stid, wdir) {
+    wdir.push(wdir[wdir.length - 1])
+    const wimg = wdir.map(d => !d ? '&nbsp;' : '&#10148;')
+    const rotate = wdir.map(d => `rotate(${d + 90}deg)`)
+    const finalElement = document.getElementById(`${stid}-wdir-${wdir.length - 1}`)
+    for (let i=0; i<wdir.length; i++) {
+        const element = document.getElementById(`${stid}-wdir-${i}`)
+        element.innerHTML = wimg[i]
+        element.style.transform = rotate[i]
+    }
+};
+
+function windBarHeight(stid, wspd, gust, multiplier) {
+    if (Math.max(...gust) > 30) multiplier = 1.5
+    else multiplier = stid === 'tile' ? 2.5 : 4
+    for (let i=0; i<wspd.length; i++) {
+        document.getElementById(`${stid}-wbar-${i}`).className = wspd[i] ? 'border-1 border' : ''
+        document.getElementById(`${stid}-wbar-${i}`).style.height = `${wspd[i] * multiplier}px`
+        document.getElementById(`${stid}-gbar-${i}`).style.height = `${(gust[i] - wspd[i]) * multiplier}px`
+    }
+};
+
+function barHeightRange(data, pxMin, pxMax) {
+    const min = Math.min(...data)
+    const max = Math.max(...data)
+    return data.map(d => Math.round(((d - min) * pxMax)/(max - min)) + pxMin)
+};
+
+function windBarColor(stid, data) {
+    const yellow = (stid==='AMB' || stid==='OGP') ? 20 : stid==='FPS' ? 15 : 10
+    const red = (stid==='AMB' || stid==='OGP') ? 30 : 20
+    const barColor = data.map(d => (d > yellow && d < red) ? 'var(--bs-yellow)' : d >= red ? 'var(--bs-orange)' : 'var(--bs-teal)')
+    for (let i=0; i<data.length; i++) {
+        document.getElementById(`${stid}-wbar-${i}`).style.backgroundColor = barColor[i]
+    }
+};
+
+function windSpeed(stid, wspd) {
+    wspd.push(wspd[wspd.length - 1])
+    wspd = wspd.map(d => d === null ? '&nbsp;' : d < 0.5 ? 'Calm' : Math.round(d))
+    if (stid !== 'tile') {
+        for (let i=0; i<wspd.length; i++) {
+            const element = document.getElementById(`${stid}-wspd-${i}`)
+            if (wspd[i] === 'Calm' && i !== wspd.length - 1) element.className = 'fs-3 fw-normal'
+            if (wspd[i] === 'Calm' && i === wspd.length - 1) element.className = 'align-self-end fs-1 text-center'
+            element.innerHTML = wspd[i]
         }
     }
-})();
+    else if (wspd[wspd.length - 1] === '&nbsp;') document.getElementById(`${stid}-wspd-12`).style.display = 'none'
+    else document.getElementById(`${stid}-wspd-12`).innerHTML = wspd[wspd.length - 1]
+};
 
-function kslcAltiTempZone(data, time=[], alti=[], temp=[]) {
-    const latestAlti = parseFloat(data.altimeter_set_1.slice(-1)).toFixed(2)
-    const latestTemp = Math.round(data.air_temp_set_1.slice(-1))
-    let latestZone = calculateZone(latestAlti, latestTemp)
-    const zoneColor = (latestZone===0 || latestZone===7) ? wwRed : (latestZone===1 || latestZone===6) ? wwOrg : (latestZone===2 || latestZone===5) ? wwYlw : wwGrn
-    latestZone = latestZone===0 ? '&#9471;' : (latestZone==='LoP') ? 'LoP' : `&#1010${latestZone+1}`
-    document.getElementById('KSLC-zone').innerHTML = latestZone
-    document.getElementById('KSLC-zone').style.color = zoneColor
-    for (let i=0; i<data.date_time.length; i++) {
-        if (data.date_time[i].slice(-5,-3)==='00') {
-            time.push(data.date_time[i].toLowerCase().replace(/:\d{2}/g, ''))
-            temp.push(`${Math.round(data.air_temp_set_1[i])}&deg;`)
-            alti.push(data.altimeter_set_1[i].toFixed(2))
-        }
+function windGust(stid, gust) {
+    gust.push(gust[gust.length - 1])
+    gust = gust.map(d => !d ? '&nbsp;' : `g${Math.round(d)}`)
+    if (stid !== 'tile') {
+        for (let i=0; i<gust.length; i++) document.getElementById(`${stid}-gust-${i}`).innerHTML = gust[i]
     }
-    const hourlyData = {'time':time.slice(-6), 'temp':temp.slice(-6), 'alti':alti.slice(-6)}
-    zone(hourlyData)
-}
+    else if (gust[gust.length - 1] === '&nbsp;') document.getElementById(`${stid}-gust-12`).style.display = 'none'
+    else document.getElementById(`${stid}-gust-12`).innerHTML = gust[gust.length - 1]
+};
 
-function calculateZone(alti, temp, currentZones = []) {
+function zone(temp, alti, time, count = 3, barHeight = [], start) {
+    const timeMarker = time[time.length - 1].slice(-5,-3)
+    for (let i=time.length - 1; i>0; i--) {
+        if (time[i].slice(-5,-3) === timeMarker) {
+            barHeight.push(alti[i])
+            if (count === 0) start = time[i].toLowerCase()
+            const zone = calculateZone(alti[i], temp[i])
+            document.getElementById(`alti-${count}`).innerHTML = (alti[i]).toFixed(2)
+            document.getElementById(`zone-${count}`).innerHTML = zone.num
+            document.getElementById(`zone-${count}`).style.color = zone.col
+            if (zone.num === 'LoP') document.getElementById(`zone-${count}`).className = 'fs-2 fw-semibold negative-vert-pad'
+            count --
+        }
+        if (count < 0) break
+    }
+    barHeight = barHeightRange(barHeight, 0, 20).reverse()
+    for (let i=0; i<barHeight.length; i++) {
+        document.getElementById('temp-main').style.display = 'block'
+        document.getElementById(`altibar-${i}`).style.height = `${barHeight[i]}px`
+    }
+    document.getElementById('zone-time-range').innerHTML = `${start} - ${time[time.length - 1].toLowerCase()} @KSLC`
+};
+
+function calculateZone(alti, temp, currentZones = [], zone = {}) {
+    // case 0..<(-0.000714 * airTemp + 29.9214):
+    //   return("Zone 0")
+    // case (-0.000714 * airTemp + 29.9214)..<(-0.001714 * airTemp + 30.0514):
+    //   return("Zone 1")
+    // case (-0.001714 * airTemp + 30.0514)..<(-0.001714 * airTemp + 30.1114):
+    //   return("Zone 2L")
+    // case (-0.001714 * airTemp + 30.1114)..<(-0.003 * airTemp + 30.27):
+    //   return("Zone 3-")
+    // case (-0.003 * airTemp + 30.27)..<(-0.004286 * airTemp + 30.4286):
+    //   return("Zone 4+")
+    // case (-0.004286 * airTemp + 30.4286)..<(-0.004286 * airTemp + 30.4886):
+    //   return("Zone 5H")
+    // case (-0.004286 * airTemp + 30.4886)..<(-0.005429 * airTemp + 30.6228):
+    //   return("Zone 6")
+    // default:
+    //   return("Zone 7")
     const zoneSlope = [0.05, 0.12, 0.19, 0.33, 0.47, 0.54, 0.62, -1]
     const zoneIntercept = [29.91, 30.01, 30.11, 30.27, 30.43, 30.53, 30.65, 100]
     for (let i=0; i<zoneSlope.length; i++) currentZones.push(Math.round((zoneSlope[i]/-110*temp+zoneIntercept[i])*100)/100)
-    const zone = currentZones.findIndex(d => d >= alti)
-    return alti===currentZones[3] ? 'LoP' : zone
-}
+    zone.num = currentZones.findIndex(d => d >= alti)
+    if (zone.num === 0 || zone.num === 7) zone.col = 'var(--bs-red)'
+    else if (zone.num===1 || zone.num===6) zone.col = 'var(--bs-orange)'
+    else if (zone.num===2 || zone.num===5) zone.col = 'var(--bs-yellow)'
+    else zone.col = 'var(--bs-teal)'
+    zone.num = alti == currentZones[3] ? 'LoP' : zone.num
+    zone.num = zone.num === 0 ? '&#9471;' : (zone.num === 'LoP') ? 'LoP' : `&#1010${zone.num + 1};`
+    return zone
+};
 
-function zone(data, zDigit=[]) {
-    for (let i=0; i<data.alti.length; i++) zDigit.push(calculateZone(parseFloat(data.alti[i]), parseInt(data.temp[i])))
-    const zColor = zDigit.map(d => (d===0 || d===7) ? wwRed : (d===1 || d===6) ? wwOrg : (d===2 || d===5) ? wwYlw : wwGrn)
-    const zFormatted = zDigit.map(d => d===0 ? '&#9471;' : d==='LoP' ? '<span class="display-3 fw-bold">LoP</span>' : `&#1010${d+1}`)
-    const min = Math.min(...data.alti)
-    const max = Math.max(...data.alti)
-    const barHeight = data.alti.map(d => `${(((d-min)*80)/(max-min))+10}px`)
-    for (let i=0; i<6; i++) {
-        const element = document.getElementById(`KSLC-zone-${i}`)
-        element.innerHTML = zFormatted[i]
-        element.style.color = zColor[i]
-        document.getElementById(`KSLC-alti-${i}`).innerHTML = data.alti[i]
-        document.getElementById(`KSLC-temp-${i}`).innerHTML = data.temp[i]
-        document.getElementById(`KSLC-alti-time-${i}`).innerHTML = data.time[i]
-        document.getElementById(`KSLC-altibar-${i}`).style.height = barHeight[i]
+function windChart(stid, data) {
+    const sliceLength = data.date_time.length < 12 ? 6 : 12
+    windDirection(stid, data.wind_direction_set_1.slice(-sliceLength))
+    windBarHeight(stid, data.wind_speed_set_1.slice(-sliceLength), data.wind_gust_set_1.slice(-sliceLength))
+    windBarColor(stid, data.wind_speed_set_1.slice(-sliceLength))
+    windSpeed(stid, data.wind_speed_set_1.slice(-sliceLength))
+    windGust(stid, data.wind_gust_set_1.slice(-sliceLength))
+    time(stid, data.date_time.slice(-sliceLength))
+    document.getElementById(`${stid}-main`).style.display = 'block'
+};
+
+function time(stid, time) {
+    time.push(time[time.length - 1])
+    for (let i=0; i<time.length; i++) {
+        const element = document.getElementById(`${stid}-time-${i}`)
+        if (i === time.length - 1) element.innerHTML = time[i].toLowerCase()
+        else element.innerHTML = stid === 'AMB' ? time[i].replace(':00','').toLowerCase() : time[i].slice(0,-3)
     }
-}
-
-function windChart(data) {
-    const ylwLim = (data.stid==='AMB' || data.stid==='OGP') ? 19 : data.stid==='FPS' ? 15 : 9
-    const redLim = (data.stid==='AMB' || data.stid==='OGP') ? 29 : 19
-    const length = data.stid==='AMB' ? 6 : 12
-    document.getElementById(`${data.stid}-main`).style.display = 'block'
-    for (let key in data) data[key] = data[key].slice(-length)
-    time(data.stid, data.date_time)
-    wind(data.stid, data.wind_speed_set_1, ylwLim, redLim)
-    if (data.wind_direction_set_1) wdir(data.stid, data.wind_direction_set_1)
-    if (data.wind_gust_set_1) gust(data.stid, data.wind_gust_set_1, data.wind_speed_set_1)
-    else { for (let i=0; i<length; i++) { document.getElementById(`${data.stid}-gust-${i}`).innerHTML = '&nbsp;' } }
-}
-
-function time(stid, data) {
-    document.getElementById(`${stid}-time`).innerHTML = data[data.length-1].toLowerCase()
-    data = data.map(d => data.length<11 ? d.toLowerCase().replace(':00', '') : d.toLowerCase().slice(0,-3))
-    for (let i=0; i<data.length; i++) document.getElementById(`${stid}-time-${i}`).innerHTML = data[i]
-}
-
-function wind(stid, data, ylwLim, redLim) {
-    data = data.map(d => Math.round(d)>=1 ? Math.round(d) : d===null ? '&nbsp;' : '<span class="fs-3 fw-normal">Calm</span>')
-    const barHeight = data.map(d => d!=='' ? `${d*4}px` : '0px')
-    const barColor = data.map(d => (d>ylwLim && d<redLim) ? wwYlw : d>=redLim ? wwOrg : wwGrn)
-    document.getElementById(`${stid}-wind`).innerHTML = typeof data[data.length-1]==='string' ? '<span class="display-5 fw-bold">Calm</span>' : data[data.length-1]
-    for (let i=0; i<data.length; i++) {
-        document.getElementById(`${stid}-wind-${i}`).innerHTML = data[i]
-        let element = document.getElementById(`${stid}-wbar-${i}`)
-        element.style.height = barHeight[i]
-        element.style.backgroundColor = barColor[i]
-    }
-}
-
-function wdir(stid, data) {
-    const wimg = data.map(d => d>0 ? '&#10148;' : '&nbsp;')
-    const wdir = data.map(d => d>0 ? `rotate(${d+90}deg)` : '')
-    document.getElementById(`${stid}-wdir`).innerHTML = wimg[wimg.length-1]
-    document.getElementById(`${stid}-wdir`).style.transform = wdir[wdir.length-1]
-    for (let i=0; i<data.length; i++) {
-        const element = document.getElementById(`${stid}-wdir-${i}`)
-        element.innerHTML = wimg[i]
-        element.style.transform = wdir[i]
-    }
-}
-
-function gust(stid, data, wind, barHeight=[]) {
-    for (let i=0; i<data.length; i++) barHeight.push(data[i]>=1 ? `${(data[i]-wind[i])*4}px` : '0px')
-    data = data.map(d => d>=1 ? `g${Math.round(d)}` : '&nbsp;')
-    if (data[data.length-1]!=='&nbsp;') {
-        document.getElementById(`${stid}-gust`).innerHTML = data[data.length-1]
-        document.getElementById(`${stid}-gust`).style.display = 'block'
-    }
-    for (let i=0; i<data.length; i++) {
-        document.getElementById(`${stid}-gust-${i}`).innerHTML = data[i]
-        document.getElementById(`${stid}-gbar-${i}`).style.height = barHeight[i]
-    }
-}
+};
