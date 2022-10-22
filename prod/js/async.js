4f972e04dc6a4e77c18bcac59e708e2add3ee3d5
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-10-22 10:20:04 -0600

    Update async.js

--- prod/js/async.js
+++ prod/js/async.js
@@ -41,10 +41,11 @@
         catch (error) { console.log('Sounding data fetch failed') }
         if (maxTempF && soundingData) {
             document.getElementById('max-temp').innerHTML = `${maxTempF}&deg;`
-            console.log('test')
             liftParams = getLiftParams(maxTempF, soundingData)
-            document.getElementById('neg3').innerHTML = Math.round(liftParams.neg3 * 3.28084).toLocaleString()
-            document.getElementById('tol').innerHTML = Math.round(liftParams.tol * 3.28084).toLocaleString()
+            if (liftParams !== null) {
+                document.getElementById('neg3').innerHTML = Math.round(liftParams.neg3 * 3.28084).toLocaleString()
+                document.getElementById('tol').innerHTML = Math.round(liftParams.tol * 3.28084).toLocaleString()
+            }
             decodedSkewTChart(maxTempF, soundingData, liftParams)
         }
     } catch (error) { console.log(error) }
