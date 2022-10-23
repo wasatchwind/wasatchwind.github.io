5ea87b1202e0d78319e9eb15dad26958e24ff56f
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-10-23 14:32:06 -0600

    Update async.js

--- prod/js/async.js
+++ prod/js/async.js
@@ -44,7 +44,7 @@
         if (maxTempF && soundingData) {
             liftParams = getLiftParams(maxTempF, soundingData)
             document.getElementById('neg3').innerHTML = liftParams.neg3 ? Math.round(liftParams.neg3 * 3.28084).toLocaleString() : '--'
-            document.getElementById('tol').innerHTML = liftParams.tol ? Math.round(liftParams.tol * 3.28084).toLocaleString() : '--'
+            document.getElementById('tol').innerHTML = Math.round(liftParams.tol * 3.28084).toLocaleString()
             decodedSkewTChart(maxTempF, soundingData, liftParams)
         }
     } catch (error) { console.log(error) }
