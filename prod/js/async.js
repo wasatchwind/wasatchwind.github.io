b15c6bc9fa1323fc391afbbffdc3bcd0a9c8a657
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2022-11-02 14:54:18 -0600

    Update async.js

--- prod/js/async.js
+++ prod/js/async.js
@@ -9,6 +9,7 @@
         const timeSeriesURL = `https://api.mesowest.net/v2/station/timeseries?&stid=KSLC&stid=UTOLY&stid=AMB&stid=KU42&stid=FPS&stid=OGP&stid=HF012&recent=420&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=${nwsToken.token}`
         const timeSeriesData = await (await fetch(timeSeriesURL)).json()
         if (timeSeriesData.SUMMARY.RESPONSE_MESSAGE === 'OK') {
+            ensureWindData(timeSeriesData)
             ensureGustData(timeSeriesData)
             ensureDirData(timeSeriesData)
             if (timeSeriesData.STATION[0].STID === 'KSLC') kslcTiles(timeSeriesData.STATION[0].OBSERVATIONS)
