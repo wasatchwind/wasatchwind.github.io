afae096799b65537e2f2d0c9e4af90069fa97564
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-02-26 19:50:20 -0700

    Update async.js

--- prod/js/async.js
+++ prod/js/async.js
@@ -7,7 +7,6 @@
         const nwsTokenURL = 'https://us-west3-wasatchwind.cloudfunctions.net/nws-token-1'
         try { nwsToken = await (await fetch(nwsTokenURL)).json() }
         catch (error) { console.log('NWS token fetch failed') }
-        console.log(nwsToken)
         const timeSeriesURL = `https://api.mesowest.net/v2/station/timeseries?&stid=KSLC&stid=UTOLY&stid=AMB&stid=KU42&stid=FPS&stid=OGP&stid=HF012&recent=420&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=${nwsToken.token}`
         const timeSeriesData = await (await fetch(timeSeriesURL)).json()
         if (timeSeriesData.SUMMARY.RESPONSE_MESSAGE === 'OK') {
