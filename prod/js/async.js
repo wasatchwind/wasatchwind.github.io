ff1de574666fe8d3cc3e34c1ed7a2628185a69f1
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-03-26 17:58:52 -0600

    Update async.js

--- prod/js/async.js
+++ prod/js/async.js
@@ -105,7 +105,9 @@
         processAreaForecast(areaForecastText)
     } catch (error) { console.log(error) }
 
-    if (now.getHours() > 5 && now.getHours() < 17) windSurfaceForecastGraphical()
+    if (now.getHours() > 5 && now.getHours() < 17) {
+        windSurfaceForecastGraphical()
+    }
 //     document.getElementById('latest-cam').src = 'https://www.wrh.noaa.gov/images/slc/camera/latest/darren2.latest.jpg'
     document.getElementById('latest-cam').src = 'https://meso1.chpc.utah.edu/station_cameras/armstrong_cam/armstrong_cam_current.jpg'
     document.getElementById('spinner').style.display = 'none'
