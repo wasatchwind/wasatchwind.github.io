46d18691d60548e6b6cdbb885cc20eda19f512d2
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-03-26 20:34:40 -0600

    Update async.js

--- stage/prod/js/async.js
+++ stage/prod/js/async.js
@@ -5,21 +5,21 @@
     try {
         const nwsToken = await (await fetch(nwsTokenURL)).json()
         // TIMESERIES ALL STATIONS
-        const timeSeriesURL = `https://api.mesowest.net/v2/station/timeseries?&stid=KSLC&stid=UTOLY&stid=AMB&stid=KU42&stid=FPS&stid=OGP&stid=HF012&recent=420&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=${nwsToken.token}`
-        try {
-            const timeSeriesData = await (await fetch(timeSeriesURL)).json()
-            if (timeSeriesData.SUMMARY.RESPONSE_MESSAGE === 'OK') {
-                ensureWindData(timeSeriesData)
-                ensureGustData(timeSeriesData)
-                ensureDirData(timeSeriesData)
-                if (timeSeriesData.STATION[0].STID === 'KSLC') kslcTiles(timeSeriesData.STATION[0].OBSERVATIONS)
-                for (let i=0; i<timeSeriesData.STATION.length; i++) windChart(timeSeriesData.STATION[i].STID, timeSeriesData.STATION[i].OBSERVATIONS)
-            }
-            else throw(console.log('Timeseries fetch failed'))
-        } catch (error) {
-            document.getElementById('tile-wspd-12').innerHTML = 'Timeseries data error<br>Refresh or try again later'
-            document.getElementById('tile-wspd-12').className = 'fs-2'
-        }
+//         const timeSeriesURL = `https://api.mesowest.net/v2/station/timeseries?&stid=KSLC&stid=UTOLY&stid=AMB&stid=KU42&stid=FPS&stid=OGP&stid=HF012&recent=420&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=${nwsToken.token}`
+//         try {
+//             const timeSeriesData = await (await fetch(timeSeriesURL)).json()
+//             if (timeSeriesData.SUMMARY.RESPONSE_MESSAGE === 'OK') {
+//                 ensureWindData(timeSeriesData)
+//                 ensureGustData(timeSeriesData)
+//                 ensureDirData(timeSeriesData)
+//                 if (timeSeriesData.STATION[0].STID === 'KSLC') kslcTiles(timeSeriesData.STATION[0].OBSERVATIONS)
+//                 for (let i=0; i<timeSeriesData.STATION.length; i++) windChart(timeSeriesData.STATION[i].STID, timeSeriesData.STATION[i].OBSERVATIONS)
+//             }
+//             else throw(console.log('Timeseries fetch failed'))
+//         } catch (error) {
+//             document.getElementById('tile-wspd-12').innerHTML = 'Timeseries data error<br>Refresh or try again later'
+//             document.getElementById('tile-wspd-12').className = 'fs-2'
+//         }
         // TIMESERIES KSLC ONLY FOR ZONE CHART
         const kslcZoneDataURL = `https://api.mesowest.net/v2/station/timeseries?&stid=KSLC&recent=800&vars=air_temp,altimeter&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=${nwsToken.token}`
         try {
