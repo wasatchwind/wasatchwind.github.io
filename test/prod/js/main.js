699f1203517da98c01c41d6e41ee775d795d3e52
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-11-15 14:22:14 -0700

    Update main.js

--- test/prod/js/main.js
+++ test/prod/js/main.js
@@ -1,36 +1,4 @@
 'use strict';
-const menuItems = document.querySelectorAll('.carousel .carousel-item')
-let slideCount = 3, menuOrder = [], hour = new Date().getHours()
-menuOrder = hour > 12 ? ['Tomorrow', 'Now', 'Today'] : ['Now', 'Today', 'Tomorrow']
-
-document.getElementById('menu-left').innerHTML = menuOrder[0]
-document.getElementById('menu-center').innerHTML = menuOrder[1]
-document.getElementById('menu-right').innerHTML = menuOrder[2]
-document.getElementById('show-page').innerHTML = menuOrder[1]
-
-menuItems.forEach((element) => {
-  let next = element.nextElementSibling
-  for (var i=1; i<slideCount; i++) {
-    if (!next) next = menuItems[0]
-    let cloneChild = next.cloneNode(true)
-    element.appendChild(cloneChild.children[0])
-    next = next.nextElementSibling
-  }
-})
-
-document.getElementById('menu').addEventListener('slide.bs.carousel', function (e) {
-  switch(e.to) {
-    case 0:
-      console.log(menuOrder[1])
-      document.getElementById('show-page').innerHTML = menuOrder[1]
-      break
-    case 1:
-      console.log(menuOrder[2])
-      document.getElementById('show-page').innerHTML = menuOrder[2]
-      break
-    case 2:
-      console.log(menuOrder[0])
-      document.getElementById('show-page').innerHTML = menuOrder[0]
-      break
-  }
-})
+function toggleDiv(selected) {
+  console.log(selected)
+}
