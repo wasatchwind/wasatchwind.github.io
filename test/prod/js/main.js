a11ca78613f8e82731d410dfb357e16fab6a78e6
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-11-17 11:17:48 -0700

    Update main.js

--- test/prod/js/main.js
+++ test/prod/js/main.js
@@ -1,4 +1,55 @@
 'use strict';
-function toggleDiv(selected) {
-  console.log(selected)
+const wrapper = document.querySelector('.wrapper')
+const carousel = document.querySelector('.carousel')
+const firstCardWidth = carousel.querySelector('.tile').offsetWidth
+const carouselChildrens = [...carousel.children]
+let isDragging = false, startX, startScrollLeft
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML('afterbegin', card.outerHTML)
+})
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML('beforeend', card.outerHTML)
+})
+// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+carousel.classList.add('no-transition')
+carousel.scrollLeft = carousel.offsetWidth
+carousel.classList.remove('no-transition')
+const dragStart = (e) => {
+    isDragging = true
+    carousel.classList.add('dragging')
+    // Records the initial cursor and scroll position of the carousel
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft
 }
+const dragging = (e) => {
+    if(!isDragging) return // if isDragging is false return from here
+    // Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
+}
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove('dragging')
+}
+const infiniteScroll = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel.scrollLeft === 0) {
+        carousel.classList.add('no-transition')
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth)
+        carousel.classList.remove('no-transition')
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+        carousel.classList.add('no-transition')
+        carousel.scrollLeft = carousel.offsetWidth
+        carousel.classList.remove('no-transition')
+    }
+}
+
+carousel.addEventListener('mousedown', dragStart)
+carousel.addEventListener('mousemove', dragging)
+document.addEventListener('mouseup', dragStop)
+carousel.addEventListener('scroll', infiniteScroll)
