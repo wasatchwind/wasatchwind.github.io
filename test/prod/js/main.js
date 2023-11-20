c588ce2e00bc75139f819342a48bde5aa8c1ec6b
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-11-20 16:33:14 -0700

    Update main.js

--- test/prod/js/main.js
+++ test/prod/js/main.js
@@ -1,55 +1,23 @@
 'use strict';
-const wrapper = document.querySelector('.wrapper')
-const carousel = document.querySelector('.carousel')
-const firstCardWidth = carousel.querySelector('.tile').offsetWidth
-const carouselChildrens = [...carousel.children]
-let isDragging = false, startX, startScrollLeft
-// Get the number of cards that can fit in the carousel at once
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth)
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML('afterbegin', card.outerHTML)
-})
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML('beforeend', card.outerHTML)
-})
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add('no-transition')
-carousel.scrollLeft = carousel.offsetWidth
-carousel.classList.remove('no-transition')
-const dragStart = (e) => {
-    isDragging = true
-    carousel.classList.add('dragging')
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX
-    startScrollLeft = carousel.scrollLeft
-}
-const dragging = (e) => {
-    if(!isDragging) return // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
-}
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove('dragging')
-}
-const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add('no-transition')
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth)
-        carousel.classList.remove('no-transition')
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add('no-transition')
-        carousel.scrollLeft = carousel.offsetWidth
-        carousel.classList.remove('no-transition')
-    }
-}
+console.log('test')
 
-carousel.addEventListener('mousedown', dragStart)
-carousel.addEventListener('mousemove', dragging)
-document.addEventListener('mouseup', dragStop)
-carousel.addEventListener('scroll', infiniteScroll)
+$(document).ready(function(){
+  $("#carousel-nav").owlCarousel({
+    items: 3, // Number of cards shown in each slide
+    loop: true, // Continuously loop the carousel
+    margin: 20, // Space between cards
+    nav: true, // Show navigation buttons
+    navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"], // Custom navigation icons
+    responsive: {
+      0: {
+        items: 1 // Number of cards shown in the carousel for smaller screens
+      },
+      768: {
+        items: 2 // Number of cards shown in the carousel for medium screens
+      },
+      992: {
+        items: 3 // Number of cards shown in the carousel for large screens
+      }
+    }
+  });
+});
