eb9b1d89e468a93325e250f2173408708c8efb3e
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-11-21 12:25:26 -0700

    Update main.js

--- test/prod/js/main.js
+++ test/prod/js/main.js
@@ -1,17 +1,10 @@
-'use strict';
-console.log('test')
-
-// https://spider149.github.io/own-carousel/
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".own-carousel__container").ownCarousel({
-      itemPerRow:3, 
-      itemWidth:30,
-      responsive: {
-          1000: [4, 24],
-          800: [3, 33],
-          600: [2, 49],
-          400: [1, 100]
-      },
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 3,
+    margin: 10,
+    loop: true,
+    nav: true
   });
-  responsive();
 });
