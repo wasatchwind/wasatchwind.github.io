1f9b83f552a48c905688d7ce405d1773b8e03042
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-11-26 16:12:31 -0700

    Create main.js

---
+++ test2/main.js
@@ -0,0 +1,7 @@
+'use strict';
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".own-carousel__container").ownCarousel({
+      itemPerRow:3, 
+      itemWidth:32
+  })
+})
