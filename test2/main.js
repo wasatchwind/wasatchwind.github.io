454f56b30557c825e4535ddec30ea03189325f20
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2023-11-26 17:09:31 -0700

    Add files via upload

--- test2/main.js
+++ test2/main.js
@@ -1,4 +1,6 @@
 'use strict';
+
+// https://spider149.github.io/own-carousel/
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".carousel-container").ownCarousel({
       itemPerRow:3, 
