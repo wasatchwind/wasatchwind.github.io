dc7a68ace04c6c37dbf807f57ad69657ba79b8ed
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2024-01-14 20:37:20 -0700

    Add files via upload

--- test2/main.js
+++ test2/main.js
@@ -1,9 +1,22 @@
 'use strict';
 
-// https://spider149.github.io/own-carousel/
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".carousel-container").ownCarousel({
-      itemPerRow:3, 
-      itemWidth:32
-  })
+const slider = new KeenSlider('#my-keen-slider', { //https://keen-slider.io/docs
+  loop: true,
+  mode: 'free-snap',
+  slides: {
+    perView: 3,
+  },
+  animationEnded: () => {
+    console.log(navItems[slider.track.details.rel])
+    document.getElementById('content').innerHTML = navItems[slider.track.details.rel]
+  },
+  dragSpeed: 0.25,
 })
+
+const now = new Date()
+const tomorrow = new Date(now.setDate(now.getDate() + 1)).toLocaleString('en-us', {weekday: 'long'})
+const navItems = ['2:45 pm', 'Today', tomorrow, 'Longterm', 'Cams', 'GPS', 'Settings']
+for (let i=0; i<navItems.length; i++) {
+  document.getElementById(i).innerHTML = navItems[i]
+}
+document.getElementById('content').innerHTML = navItems[1]
