70cc7faa570f2df9a7f6a1f8a690590a8567a360
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2024-01-15 15:20:32 -0700

    Add files via upload

--- test2/main.js
+++ test2/main.js
@@ -1,15 +1,22 @@
 'use strict';
 const now = new Date()
-const tomorrow = new Date(now.setDate(now.getDate() + 1)).toLocaleString('en-us', {weekday: 'long'})
-const navItems = ['Now', 'Today', tomorrow, 'Longterm', 'Cams', 'GPS']
+const tomorrow = new Date(now.setDate(now.getDate() + 1)).toLocaleString('en-us', {weekday: 'short'})
+const navItems = ['Now', 'Today', tomorrow, 'Long', 'Cams', 'GPS']
 for (let i=0; i<navItems.length; i++) {
   document.getElementById(`nav-${i}`).innerHTML = navItems[i]
 }
 document.getElementById('content').innerHTML = navItems[0]
+document.getElementById('nav-0').style.color = 'yellow'
 
 function reload() {
   history.scrollRestoration = 'manual'
   location.reload()
+}
+
+function resetNavColors () {
+  for (let i=0; i<navItems.length; i++) {
+    document.getElementById(`nav-${i}`).style.color = 'white'
+  }
 }
 
 // Menu navigation carousel/slider (https://keen-slider.io/docs)
@@ -22,6 +29,8 @@ const slider = new KeenSlider('#slider', {
   animationEnded: () => {
     console.log(navItems[slider.track.details.rel])
     document.getElementById('content').innerHTML = navItems[slider.track.details.rel]
+    resetNavColors()
+    document.getElementById(`nav-${slider.track.details.rel}`).style.color = 'yellow'
   },
   dragSpeed: 0.6,
 })
