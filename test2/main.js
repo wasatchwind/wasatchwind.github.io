9888d0cdfb0cee8399cbce4fa2c4d7650a7d2cf7
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2024-01-17 13:12:35 -0700

    Add files via upload

--- test2/main.js
+++ test2/main.js
@@ -1,18 +1,32 @@
 'use strict';
 const now = new Date()
 const tomorrow = new Date(now.setDate(now.getDate() + 1)).toLocaleString('en-us', {weekday: 'short'})
-const navItems = ['Now', 'Today', tomorrow, 'Long', 'Cams', 'GPS']
 
 function reload() {
   history.scrollRestoration = 'manual'
   location.reload()
 }
 
+// Set nav item order according to time of day
+let navItems = ['Now', 'Today', tomorrow, 'Long', 'Cams', 'GPS']
+if (now.getHours() < 14) {
+  navItems.push(navItems[0])
+  navItems.shift()
+}
+
 // Set nav item labels & active label
 let activeNav = 0
 document.getElementById(`nav-${activeNav}`).style.color = 'white'
+document.getElementById(`${navItems[activeNav]}`).style.display = 'block'
 for (let i=0; i<navItems.length; i++) {
   document.getElementById(`nav-${i}`).innerHTML = navItems[i]
+}
+
+// Update nav colors and page shown
+function navUpdate (navElement, color, pageId, status) {
+  document.getElementById(navElement).style.color = color
+  if (pageId === tomorrow) pageId = 'Tomorrow'
+  document.getElementById(pageId).style.display = status
 }
 
 // Menu navigation carousel/slider (https://keen-slider.io/docs)
@@ -22,9 +36,8 @@ const slider = new KeenSlider('#slider', {
     perView: 3,
   },
   animationEnded: () => {
-    document.getElementById(`nav-${activeNav}`).style.color = 'var(--bs-secondary)'
+    navUpdate(`nav-${activeNav}`, 'var(--bs-secondary)', navItems[activeNav], 'none')
     activeNav = slider.track.details.rel
-    document.getElementById(`nav-${activeNav}`).style.color = 'white'
-    console.log(navItems[slider.track.details.rel])
+    navUpdate(`nav-${activeNav}`, 'white', navItems[activeNav], 'block')
   },
 })
