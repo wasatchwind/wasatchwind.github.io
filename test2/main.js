7026aa18d7707f4d3f8611a30d8f6444a779b470
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2024-02-04 17:41:40 -0700

    Add files via upload

--- test2/main.js
+++ test2/main.js
@@ -22,7 +22,7 @@ for (let i=0; i<navItems.length; i++) {
   document.getElementById(`nav-${i}`).innerHTML = navItems[i]
 };
 
-// Update nav colors and page shown
+// Update nav colors and visible page
 function navUpdate (navElement, color, pageId, status) {
   document.getElementById(navElement).style.color = color
   if (pageId === tomorrow) pageId = 'Tomorrow'
@@ -42,7 +42,7 @@ const slider = new KeenSlider('#slider', {
   },
 });
 
-// Marquee
+// Marquee slider (https://keen-slider.io/docs)
 const animation = { duration: 1500, easing: (t) => t }
 const marquee = new KeenSlider("#marquee", {
   loop: true,
@@ -60,8 +60,7 @@ const marquee = new KeenSlider("#marquee", {
   },
 });
 
-
-// Reveal or collapse wind charts
+// Reveal/collapse toggle for wind charts
 function toggleWindChart(div) {
   const element = document.getElementById(div)
   if (element.style.display==='' || element.style.display==='none') {
@@ -74,104 +73,7 @@ function toggleWindChart(div) {
   }
 };
 
-// Synoptic time series (https://docs.synopticdata.com/services/time-series)
-(async () => {
-  const timeSeriesURL = 'https://api.synopticdata.com/v2/station/timeseries?&stid=KSLC&stid=UTOLY&stid=AMB&stid=KU42&stid=FPS&stid=OGP&stid=HF012&recent=720&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=f8258474e4a348ceb3192e4d205f71da'
-  const timeSeriesData = await (await fetch(timeSeriesURL)).json()
-  console.log(timeSeriesData)
-  if (timeSeriesData.SUMMARY.RESPONSE_MESSAGE === 'OK') {
-    document.getElementById('last-loaded').innerHTML = `Data @ ${timeSeriesData.STATION[0].OBSERVATIONS.date_time[timeSeriesData.STATION[0].OBSERVATIONS.date_time.length-1].toLowerCase()}`
-    for (let i=0; i<timeSeriesData.STATION.length; i++) windChart(timeSeriesData.STATION[i].STID, timeSeriesData.STATION[i].OBSERVATIONS)
-  }
-  else console.log('Timeseries fetch failed')
-})();
-
-// Timeseries
-function windChart(stid, data) { // Only called if some data exists but may be partial
-  if (data.wind_speed_set_1.every(d => d === 0)) {
-    document.getElementById(`${stid}-main`).style.display = 'none'
-    return
-  }
-  const sliceLength = stid === 'AMB' ? 6 : 12 // Set chart data length, shorter for low frequency data
-  if (data.date_time.length < sliceLength) { // If data is less than chart length
-    const emptyArray = new Array(sliceLength - data.date_time.length).fill(null) // Create empty array for missing data
-    for (let value of Object.keys(data)) data[value] = emptyArray.concat(data[value]) // Iterate data object and join arrays to make complete
-  }
-  else for (let value of Object.keys(data)) data[value] = data[value].slice(-sliceLength) // If data more than needed, slice to make complete
-  time(stid, data.date_time) // date_time always included in data
-  if (!data.wind_direction_set_1) data.wind_direction_set_1 = new Array(12).fill(null) // If wind direction key missing, create it with blank value array
-  windDirection(stid, data.wind_direction_set_1) // With data guaranteed, even if empty, call function
-  if (!data.wind_speed_set_1) data.wind_speed_set_1 = new Array(12).fill(null) // If wind speed key missing, create it with blank value array
-  windSpeed(stid, data.wind_speed_set_1) // With data guaranteed, even if empty, call function
-  if (!data.wind_gust_set_1) data.wind_gust_set_1 = new Array(12).fill(null) // If wind gust key missing, create it with blank value array
-  windGust(stid, data.wind_gust_set_1) // With data guaranteed, even if empty, call function
-  windBarHeight(stid, data.wind_speed_set_1, data.wind_gust_set_1)
-  windBarColor(stid, data.wind_speed_set_1)
-  document.getElementById(`${stid}-main`).style.display = 'block' // Show chart
-};
-
-function time(stid, time) {
-  time.push(time[time.length - 1]) // Duplicate last item for main line display
-  time = time.map(d => d ? d.toLowerCase() : d) // Make all lowercase or leave null
-  for (let i=0; i<time.length; i++) {
-    time[i] = time[i] && i<time.length-1 ? time[i].slice(0,-3) : time[i] // If time !null remove am/pm
-    document.getElementById(`${stid}-time-${i}`).innerHTML = time[i] // Set html element
-  }
-};
-
-function windDirection(stid, wdir) {
-  wdir.push(wdir[wdir.length - 1]) // Duplicate last item for main line display
-  const wimg = wdir.map(d => !d ? '&nbsp;' : '&#10148;') // If null make space all else arrow character
-  const rotate = wdir.map(d => `rotate(${d + 90}deg)`) // Set rotation for wind direction
-  for (let i=0; i<wdir.length; i++) {
-    const element = document.getElementById(`${stid}-wdir-${i}`) // Base html element
-    element.innerHTML = wimg[i] // Set spaces and/or arrow characters
-    element.style.transform = rotate[i] // Rotate
-  }
-};
-
-function windSpeed(stid, wspd) {
-  wspd.push(wspd[wspd.length - 1])
-  wspd = wspd.map(d => d === null ? '&nbsp;' : d < 0.5 ? 'Calm' : Math.round(d))
-  if (stid !== 'tile') {
-    for (let i=0; i<wspd.length; i++) {
-      const element = document.getElementById(`${stid}-wspd-${i}`)
-      if (wspd[i] === 'Calm' && i !== wspd.length - 1) element.className = 'fs-3 fw-normal'
-      if (wspd[i] === 'Calm' && i === wspd.length - 1) element.className = 'align-self-end fs-1 text-center'
-      element.innerHTML = wspd[i]
-    }
-  }
-  else if (wspd[wspd.length - 1] === '&nbsp;') document.getElementById(`${stid}-wspd-12`).style.display = 'none'
-  else document.getElementById(`${stid}-wspd-12`).innerHTML = wspd[wspd.length - 1]
-};
-
-function windGust(stid, gust) {
-  gust.push(gust[gust.length - 1])
-  gust = gust.map(d => !d ? '&nbsp;' : `g${Math.round(d)}`)
-  if (stid !== 'tile') {
-    for (let i=0; i<gust.length; i++) document.getElementById(`${stid}-gust-${i}`).innerHTML = gust[i]
-  }
-  else if (gust[gust.length - 1] === '&nbsp;') document.getElementById(`${stid}-gust-12`).style.display = 'none'
-  else document.getElementById(`${stid}-gust-12`).innerHTML = gust[gust.length - 1]
-};
-
-function windBarHeight(stid, wspd, gust, multiplier) {
-  wspd.pop() // FIX this so it's not necessary to do here
-  gust.pop()
-  if (Math.max(...gust) > 30) multiplier = 1.3
-  else multiplier = 4
-  for (let i=0; i<wspd.length; i++) {
-    document.getElementById(`${stid}-wbar-${i}`).className = wspd[i] ? 'border-1 border' : ''
-    document.getElementById(`${stid}-wbar-${i}`).style.height = `${wspd[i] * multiplier}px`
-    document.getElementById(`${stid}-gbar-${i}`).style.height = `${(gust[i] - wspd[i]) * multiplier}px`
-  }
-};
-
-function windBarColor(stid, data) {
-  const yellow = (stid==='AMB' || stid==='OGP') ? 20 : stid==='FPS' ? 15 : 10
-  const red = (stid==='AMB' || stid==='OGP') ? 30 : 20
-  const barColor = data.map(d => (d > yellow && d < red) ? 'var(--bs-yellow)' : d >= red ? 'var(--bs-orange)' : 'var(--bs-teal)')
-  for (let i=0; i<data.length; i++) {
-    document.getElementById(`${stid}-wbar-${i}`).style.backgroundColor = barColor[i]
-  }
-};
+function sunset(data) {
+    const sunset = new Date(openweathermapData.sys.sunset*1000).toLocaleTimeString('en-us', {hour: 'numeric', minute: '2-digit'}).slice(0,-3)
+    document.getElementById('sunset').innerHTML = sunset
+}
---
+++ test2/timeseries.js
@@ -0,0 +1,143 @@
+'use strict';
+function timeSeries(data) { // Loop through all stations with data to build wind charts
+  for (let i=0; i<data.STATION.length; i++) {
+    windChart(data.STATION[i].STID, data.STATION[i].OBSERVATIONS)
+  }
+  if (data.STATION[0].STID = 'KSLC') {
+    const kslcData = data.STATION[0].OBSERVATIONS
+    zone(kslcData.altimeter_set_1, kslcData.air_temp_set_1)
+  }
+}
+
+function windChart(stid, data) {
+  if (data.wind_speed_set_1.every(d => d === 0)) {
+    document.getElementById(`${stid}-main`).style.display = 'none'
+    return
+  }
+  const sliceLength = stid === 'AMB' ? 6 : 12 // Set chart data length, shorter for low frequency data
+  if (data.date_time.length < sliceLength) { // If data is less than chart length
+    const emptyArray = new Array(sliceLength - data.date_time.length).fill(null) // Create empty array for missing data
+    for (let value of Object.keys(data)) data[value] = emptyArray.concat(data[value]) // Iterate data object and join arrays to make complete
+  }
+  else for (let value of Object.keys(data)) data[value] = data[value].slice(-sliceLength) // If data more than needed, slice to make complete
+  time(stid, data.date_time) // date_time always included in data
+  if (!data.wind_direction_set_1) data.wind_direction_set_1 = new Array(12).fill(null) // If wind direction key missing, create it with blank value array
+  windDirection(stid, data.wind_direction_set_1) // With data guaranteed, even if empty, call function
+  if (!data.wind_speed_set_1) data.wind_speed_set_1 = new Array(12).fill(null) // If wind speed key missing, create it with blank value array
+  windSpeed(stid, data.wind_speed_set_1) // With data guaranteed, even if empty, call function
+  if (!data.wind_gust_set_1) data.wind_gust_set_1 = new Array(12).fill(null) // If wind gust key missing, create it with blank value array
+  windGust(stid, data.wind_gust_set_1) // With data guaranteed, even if empty, call function
+  windBarHeight(stid, data.wind_speed_set_1, data.wind_gust_set_1)
+  windBarColor(stid, data.wind_speed_set_1)
+  document.getElementById(`${stid}-main`).style.display = 'block' // Show chart
+};
+
+function time(stid, time) {
+  time.push(time[time.length - 1]) // Duplicate last item for main line display
+  time = time.map(d => d ? d.toLowerCase() : d) // Make all lowercase or leave null
+  for (let i=0; i<time.length; i++) {
+    time[i] = time[i] && i<time.length-1 ? time[i].slice(0,-3) : time[i] // If time !null remove am/pm
+    document.getElementById(`${stid}-time-${i}`).innerHTML = time[i] // Set html element
+  }
+};
+
+function windDirection(stid, wdir) {
+  wdir.push(wdir[wdir.length - 1]) // Duplicate last item for main line display
+  const wimg = wdir.map(d => !d ? '&nbsp;' : '&#10148;') // If null make space all else arrow character
+  const rotate = wdir.map(d => `rotate(${d + 90}deg)`) // Set rotation for wind direction
+  for (let i=0; i<wdir.length; i++) {
+    const element = document.getElementById(`${stid}-wdir-${i}`) // Base html element
+    element.innerHTML = wimg[i] // Set spaces and/or arrow characters
+    element.style.transform = rotate[i] // Rotate
+  }
+};
+
+function windSpeed(stid, wspd) {
+  wspd.push(wspd[wspd.length - 1])
+  wspd = wspd.map(d => d === null ? '&nbsp;' : d < 0.5 ? 'Calm' : Math.round(d))
+  if (stid !== 'tile') {
+    for (let i=0; i<wspd.length; i++) {
+      const element = document.getElementById(`${stid}-wspd-${i}`)
+      if (wspd[i] === 'Calm' && i !== wspd.length - 1) element.className = 'fs-3 fw-normal'
+      if (wspd[i] === 'Calm' && i === wspd.length - 1) element.className = 'align-self-end fs-1 text-center'
+      element.innerHTML = wspd[i]
+    }
+  }
+  else if (wspd[wspd.length - 1] === '&nbsp;') document.getElementById(`${stid}-wspd-12`).style.display = 'none'
+  else document.getElementById(`${stid}-wspd-12`).innerHTML = wspd[wspd.length - 1]
+};
+
+function windGust(stid, gust) {
+  gust.push(gust[gust.length - 1])
+  gust = gust.map(d => !d ? '&nbsp;' : `g${Math.round(d)}`)
+  if (stid !== 'tile') {
+    for (let i=0; i<gust.length; i++) document.getElementById(`${stid}-gust-${i}`).innerHTML = gust[i]
+  }
+  else if (gust[gust.length - 1] === '&nbsp;') document.getElementById(`${stid}-gust-12`).style.display = 'none'
+  else document.getElementById(`${stid}-gust-12`).innerHTML = gust[gust.length - 1]
+};
+
+function windBarHeight(stid, wspd, gust, multiplier) {
+  wspd.pop() // FIX this so it's not necessary to do here
+  gust.pop()
+  if (Math.max(...gust) > 30) multiplier = 1.3
+  else multiplier = 4
+  for (let i=0; i<wspd.length; i++) {
+    document.getElementById(`${stid}-wbar-${i}`).className = wspd[i] ? 'border-1 border' : ''
+    document.getElementById(`${stid}-wbar-${i}`).style.height = `${wspd[i] * multiplier}px`
+    document.getElementById(`${stid}-gbar-${i}`).style.height = `${(gust[i] - wspd[i]) * multiplier}px`
+  }
+};
+
+function windBarColor(stid, data) {
+  const yellow = (stid==='AMB' || stid==='OGP') ? 20 : stid==='FPS' ? 15 : 10
+  const red = (stid==='AMB' || stid==='OGP') ? 30 : 20
+  const barColor = data.map(d => (d > yellow && d < red) ? 'var(--bs-yellow)' : d >= red ? 'var(--bs-orange)' : 'var(--bs-teal)')
+  for (let i=0; i<data.length; i++) {
+    document.getElementById(`${stid}-wbar-${i}`).style.backgroundColor = barColor[i]
+  }
+};
+
+function calculateZone(alti, temp, currentZones = [], zone = {}) {
+  const zoneSlope = [0.0714, 0.0714, 0.0714, 0.3, 0.4286, 0.4286, 0.5429, -1]
+  const zoneIntercept = [29.9214, 30.0514, 30.1114, 30.27, 30.4286, 30.4486, 30.6228, 100]
+  for (let i=0; i<zoneSlope.length; i++) currentZones.push(Math.round((zoneSlope[i] / -110 * temp + zoneIntercept[i]) * 100) / 100)
+  zone.num = currentZones.findIndex(d => d >= alti)
+  if (zone.num === 0 || zone.num === 7) zone.col = 'var(--bs-red)'
+  else if (zone.num===1 || zone.num===6) zone.col = 'var(--bs-orange)'
+  else if (zone.num===2 || zone.num===5) zone.col = 'var(--bs-yellow)'
+  else zone.col = 'var(--bs-teal)'
+  zone.num = alti === currentZones[3] ? 'LoP' : zone.num
+  zone.num = zone.num === 0 ? '&#9471;' : (zone.num === 'LoP') ? 'LoP' : `&#1010${zone.num + 1};`
+  return zone
+};
+
+function zone(alti, temp, zone, slope, trend, altiDiff) {
+  zone = calculateZone(alti[alti.length-1], temp[temp.length-1])
+  slope = trendLine(alti)
+  altiDiff = Math.abs(Math.round((alti[alti.length-1] - alti[0]) * 100) / 100)
+  if (altiDiff > 0 && altiDiff <= 0.01) trend = 30
+  else if (altiDiff > 0.01 && altiDiff <= 0.03) trend = 60
+  else if (altiDiff > 0.03) trend = 90
+  else trend = 0
+  trend = slope > 0 ? trend * -1 : trend
+  document.getElementById('temp').innerHTML = Math.round(temp[temp.length-1])
+  document.getElementById('alti').innerHTML = alti[alti.length-1].toFixed(2)
+  document.getElementById('trend').style.transform = `rotate(${trend}deg)`
+  document.getElementById('zone').innerHTML = zone.num
+  document.getElementById('zone').style.color = zone.col
+};
+
+function trendLine(data, a=0, b=0, c=0, d=0, e=0) {
+  for (let i=0; i<data.length; i++) {
+    a = a+((i+1)*data[i])
+    b = b+data[i]
+    c = c+(i+1)**2
+    e = e+(i+1)
+  }
+  a *= data.length
+  b *= e
+  c *= data.length
+  d = e**2
+  return (a-b)/(c-d)
+};
