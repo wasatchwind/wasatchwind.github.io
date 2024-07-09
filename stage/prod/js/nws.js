e452577dad6e8db79c96e55f2a22271219022d07
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2024-07-09 12:38:27 -0600

    Update nws.js

--- stage/prod/js/nws.js
+++ stage/prod/js/nws.js
@@ -2,7 +2,7 @@
 function updateForecastElements(i, period) {
   const dayName = period.name
   const forecastText = period.detailedForecast
-  const iconUrl = `https://api.weather.gov${period.icon}`
+  const iconURL = period.icon.slice(0,5) === 'https' ? period.icon : `https://api.weather.gov${period.icon}`
   if (i === 0) {
     const forecastDayElements = document.getElementsByClassName(`forecast-day${i}-day`)
     const forecastTextElements = document.getElementsByClassName(`forecast-day${i}-txt`)
@@ -10,12 +10,12 @@ function updateForecastElements(i, period) {
     for (let j=0; j<2; j++) { //day 0 element appears 2 times in HTML
       forecastDayElements[j].innerHTML = dayName
       forecastTextElements[j].innerHTML = forecastText
-      forecastImgElements[j].src = iconUrl
+      forecastImgElements[j].src = iconURL
     }
   } else {
     document.getElementById(`forecast-day${i}-day`).innerHTML = dayName;
     document.getElementById(`forecast-day${i}-txt`).innerHTML = forecastText;
-    document.getElementById(`forecast-day${i}-img`).src = iconUrl;
+    document.getElementById(`forecast-day${i}-img`).src = iconURL;
   }
 };
 
