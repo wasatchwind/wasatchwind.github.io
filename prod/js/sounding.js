05366a44c61fff785a849975423606787081e630
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-05-06 13:30:43 -0600

    Update sounding.js

--- prod/js/sounding.js
+++ prod/js/sounding.js
@@ -52,14 +52,12 @@ function processSoaringForecast(text) {
   const neg3 = parseInt(rawForecast[12].match(/\d{4,5}/)[0]).toLocaleString()
   const soaringForecast = `${date}
   
-  Top of Lift:
-    ${topOfLift}
-  Height of -3 Index:
-    ${neg3}
+  Top of Lift: ${topOfLift}
+  Height of -3 Index: ${neg3}
   Max Rate of Lift:
-    ${rateOfLift}
-  Overdevelopment Time:
-    ${odTime}`
+  ${rateOfLift}
+  
+  Overdevelopment Time: ${odTime}`
   
   document.getElementById('soaring-forecast').innerText = soaringForecast
   document.getElementById('hi-temp').innerHTML = hiTemp
