42308e58d8d9e7e4ea23997aaa79f3244da75368
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-07-10 13:59:10 -0600

    Add files via upload

--- prod/js/sounding.js
+++ prod/js/sounding.js
@@ -198,7 +198,7 @@ function decodedSkewTChart(data, maxTemp, liftParams) {
   // Draw y axis
   svg.append('g')
     .attr('class', 'yAxis')
-    .call(d3.axisLeft(y).tickFormat(d => `${d}k'`))
+    .call(d3.axisLeft(y).tickFormat(d => `${d}k`))
   
   // Draw blank gray polygon to cover upper right grid
   const p1 = `M ${x(25)} 0, `
@@ -211,38 +211,53 @@ function decodedSkewTChart(data, maxTemp, liftParams) {
     .attr('class', 'svgbg')
     .attr('d', polygon)
   
-  // DALR label
-  svg.append('text')
-    .attr('class', 'dalrlabel')
-    .attr('transform', `rotate(45, ${x(-3)}, ${y(6)})`)
-    .style('text-anchor', 'start')
-    .text(`Dry Adiabatic Lapse Rate: -5.4° F / 1,000 ft`)
-  
   // Legend labels
   svg.append('text')
     .attr('class', 'dewpoint')
     .attr('text-anchor', 'end')
     .attr('x', x(113))
-    .attr('y', y(16.5))
+    .attr('y', y(11.5))
     .text('Dewpoint')
   svg.append('text')
     .attr('class', 'temp')
     .attr('text-anchor', 'end')
     .attr('x', x(113))
-    .attr('y', y(14.5))
-    .text('Temp')
+    .attr('y', y(10.25))
+    .text('Air Temp')
+  svg.append('text')
+    .attr('class', 'dalr')
+    .attr('text-anchor', 'end')
+    .attr('x', x(113))
+    .attr('y', y(9))
+    .text('DALR')
   
   drawDALRParams(maxTemp, liftParams)
 };
 
 function drawDALRParams (temp, params) {
-  // Legend label max temp
+  // Legend label top of lift
   svg.append('text')
-    .attr('class', 'maxtemp')
+    .attr('class', 'white')
     .attr('text-anchor', 'end')
     .attr('x', x(113))
-    .attr('y', y(18.5))
-    .text(`Max Temp DALR: ${temp}°`)
+    .attr('y', y(19))
+    .text(`Top of Lift: ${Math.round(params.tol * ftPerMeter).toLocaleString()}`)
+
+  // Legend label -3 index
+  svg.append('text')
+    .attr('class', 'white')
+    .attr('text-anchor', 'end')
+    .attr('x', x(113))
+    .attr('y', y(17))
+    .text(`-3 Index: ${Math.round(params.neg3 * ftPerMeter).toLocaleString()}`)
+
+  // Legend label max temp
+  svg.append('text')
+    .attr('class', 'white')
+    .attr('text-anchor', 'end')
+    .attr('x', x(113))
+    .attr('y', y(15))
+    .text(`@ ${temp}°`)
 
   // Max temp DALR line
   svg.append('g').append('line')
@@ -263,7 +278,7 @@ function drawDALRParams (temp, params) {
   svg.append('g').append('line')
     .attr('class', 'neg3line')
     .attr('stroke', 'white')
-    .attr('stroke-width', 2)
+    .attr('stroke-width', 3)
     .attr('x1', x((params.neg3Temp * 9 / 5) + 32))
     .attr('y1', y(params.neg3 * ftPerMeter / 1000))
     .attr('x2', x((params.neg3Temp * 9 / 5) + 32 - 5.4))
@@ -272,16 +287,9 @@ function drawDALRParams (temp, params) {
   // -3 label
   svg.append('g').append('text')
     .attr('class', 'liftlabels')
-    .attr('x', x((params.neg3Temp * 9 / 5) + 32 - 3))
-    .attr('y', y(params.neg3 * ftPerMeter / 1000 - 0.6))
+    .attr('x', x((params.neg3Temp * 9 / 5) + 32 + 2))
+    .attr('y', y(params.neg3 * ftPerMeter / 1000 - 0.3))
     .text('-3')
-  
-  // -3 height
-  svg.append('g').append('text')
-    .attr('class', 'liftheights')
-    .attr('x', x((params.neg3Temp * 9 / 5) + 32 + 4))
-    .attr('y', y(params.neg3 * ftPerMeter / 1000 - 0.5))
-    .text(Math.round(params.neg3 * ftPerMeter).toLocaleString())
 
   // Top of lift point
   svg.append('g').append('circle')
@@ -295,15 +303,8 @@ function drawDALRParams (temp, params) {
   svg.append('g').append('text')
     .attr('class', 'liftlabels')
     .attr('x', x((params.tolTemp * 9 / 5) + 32 + 2))
-    .attr('y', y(params.tol * ftPerMeter / 1000))
+    .attr('y', y(params.tol * ftPerMeter / 1000 - 0.3))
     .text('ToL')
-  
-  // Top of lift height
-  svg.append('g').append('text')
-    .attr('class', 'liftheights')
-    .attr('x', x((params.tolTemp * 9 / 5) + 32 + 10))
-    .attr('y', y(params.tol * ftPerMeter / 1000))
-    .text(`${Math.round(params.tol * ftPerMeter).toLocaleString()}`)
 };
 
 function d3Update(userLiftParams) {
@@ -316,7 +317,7 @@ function d3Update(userLiftParams) {
     outOfRange(userTemp)
     return
   }
-  if ((userLiftParams.tolTemp * 9 / 5) + 32 < -10 || userLiftParams.tol * ftPerMeter / 1000 > 20 || !userLiftParams.tol) {
+  if ((userLiftParams.tolTemp * 9 / 5) + 32 < -10 || !userLiftParams.tol) {
     outOfRange(userTemp)
   } else {
     clearChart()
@@ -343,6 +344,6 @@ function clearChart() {
   svg.select('line.neg3line').remove()
   svg.selectAll('text.liftlabels').remove()
   svg.selectAll('text.liftheights').remove()
-  svg.select('text.maxtemp').remove()
+  svg.selectAll('text.white').remove()
   svg.select('circle.tolcircle').remove()
 };
