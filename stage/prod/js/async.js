3774123960bff47880ad79ce5ec04063749f6d05
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-08-28 08:54:42 -0600

    Update async.js

--- stage/prod/js/async.js
+++ stage/prod/js/async.js
@@ -1,58 +1,138 @@
 'use strict';
-let hiTemp = null; // Global variable sourced conditionally
 
+// Function to return an API URL assembled from specific input parameters
+function buildAPIURL(params, repeatKeys = []) {
+  const query = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(params)) {
+    const isArray = Array.isArray(value);
+    const isRepeat = repeatKeys.includes(key);
+
+    if (isArray && isRepeat) {
+      // Repeated key with multiple values (e.g. stid=...&stid=...)
+      value.forEach(stid => query.append(key, stid));
+    } else {
+      // Single entry (either array to join, or single value)
+      query.set(key, isArray ? value.join(',') : value);
+    }
+  }
+  return query.toString();
+};
+
+// Function to process text for Area Forecast and Soaring Forecast
+function parsePreText(rawContent) {
+  const parser = new DOMParser()
+  const response = parser.parseFromString(rawContent, 'text/html')
+  const preElement = response.querySelector('pre')
+  return preElement.textContent
+};
+
+// Async IIFE
+// Get Open Meteo API data (documentation: https://open-meteo.com/en/docs/gfs-api)
+// Get Wind Aloft data via either AWS or GCP cloud function
 (async () => {
-  const stationsURL = buildStationURL()
-  const openmeteoURL = 'https://api.open-meteo.com/v1/gfs?latitude=40.77069&longitude=-111.96503&daily=sunset,temperature_2m_max&hourly=temperature_2m,wind_speed_10m,wind_direction_10m,wind_gusts_10m,cape,lifted_index,pressure_msl,windspeed_850hPa,windspeed_800hPa,windspeed_750hPa,windspeed_700hPa,windspeed_650hPa,windspeed_600hPa,windspeed_550hPa,winddirection_850hPa,winddirection_800hPa,winddirection_750hPa,winddirection_700hPa,winddirection_650hPa,winddirection_600hPa,winddirection_550hPa,geopotential_height_850hPa,geopotential_height_800hPa,geopotential_height_750hPa,geopotential_height_700hPa,geopotential_height_650hPa,geopotential_height_600hPa,geopotential_height_550hPa&windspeed_unit=mph&temperature_unit=fahrenheit&forecast_hours=6&forecast_days=1&timezone=America%2FDenver'
-  const windAloftURL = 'https://2kjkumjjzukwnuiomukqzexcfy0yfynp.lambda-url.us-west-1.on.aws' // AWS Lambda, uses CORS
-  // const windAloftURL = 'https://python-wind-aloft-ftp-483547589035.us-west2.run.app' // Backup GCP Cloud Run Function in case AWS fails, does not use CORS
-  const soundingURL = 'https://storage.googleapis.com/wasatch-wind-static/raob.json'
-  const soaringForecastURL = 'https://forecast.weather.gov/product.php?site=SLC&issuedby=SLC&product=SRG&format=TXT&version=1&glossary=0'
-  const timeSeriesURL = `https://api.synopticdata.com/v2/station/timeseries?${stationsURL}&recent=720&vars=air_temp,altimeter,wind_direction,wind_gust,wind_speed&units=english,speed|mph,temp|F&obtimezone=local&timeformat=%-I:%M%20%p&token=f8258474e4a348ceb3192e4d205f71da`
+  const openMeteoParams = {
+    latitude: 40.77069,
+    longitude: -111.96503,
+    daily: ['sunset', 'temperature_2m_max'],
+    hourly: [
+      'temperature_2m',
+      'wind_speed_10m',
+      'wind_direction_10m',
+      'wind_gusts_10m',
+      'cape',
+      'lifted_index',
+      'pressure_msl',
+      'windspeed_850hPa',
+      'windspeed_800hPa',
+      'windspeed_750hPa',
+      'windspeed_700hPa',
+      'windspeed_650hPa',
+      'windspeed_600hPa',
+      'windspeed_550hPa',
+      'winddirection_850hPa',
+      'winddirection_800hPa',
+      'winddirection_750hPa',
+      'winddirection_700hPa',
+      'winddirection_650hPa',
+      'winddirection_600hPa',
+      'winddirection_550hPa',
+      'geopotential_height_850hPa',
+      'geopotential_height_800hPa',
+      'geopotential_height_750hPa',
+      'geopotential_height_700hPa',
+      'geopotential_height_650hPa',
+      'geopotential_height_600hPa',
+      'geopotential_height_550hPa',
+      'temperature_800hPa'
+    ],
+    windspeed_unit: 'mph',
+    temperature_unit: 'fahrenheit',
+    forecast_hours: 6,
+    forecast_days: 1,
+    timezone: 'America/Denver'
+  };
+
+  const openMeteoURL = `https://api.open-meteo.com/v1/gfs?${buildAPIURL(openMeteoParams)}`;
+
+  // Wind Aloft data via AWS Lambda (uses CORS config in console: Configuration > Function URL)
+  const windAloftURL = 'https://2kjkumjjzukwnuiomukqzexcfy0yfynp.lambda-url.us-west-1.on.aws';
+
+  // Wind Aloft data via GCP Cloud Function (uses CORS in code)
+  // const windAloftURL = 'https://python-wind-aloft-ftp-483547589035.us-west2.run.app';
+
+  const openmeteoData = await (await fetch(openMeteoURL)).json();
+  const gcpWindAloftData = await (await fetch(windAloftURL)).json();
+
+  sunset = openmeteoData.daily.sunset[0]; // Global variable (main.js)
+  hiTemp = Math.round(openmeteoData.daily.temperature_2m_max[0]); // Global variable (main.js)
+  navSet();
+  windAloft(openmeteoData.hourly, gcpWindAloftData);
+  displayImages();
+})();
+
+// Aysnc IIFE
+// Get Synoptic Time Series API data (documentation: https://docs.synopticdata.com/services/weather-api)
+(async () => {
+  const timeSeriesURL = 'https://python-synoptic-api-483547589035.us-west3.run.app';
+  const timeSeriesData = await (await fetch(timeSeriesURL)).json();
+  timeSeries(timeSeriesData);
+})();
+
+// Async IIFE
+// Get Sounding data
+// Get Soaring data
+(async () => {
+  const soundingURL = 'https://storage.googleapis.com/wasatch-wind-static/raob.json';
+  const soaringForecastURL = 'https://forecast.weather.gov/product.php?site=SLC&issuedby=SLC&product=SRG&format=TXT&version=1';
+
+  soundingData = await (await fetch(soundingURL)).json(); // Global variable (main.js)
+  const soaringForecastPageContent = await (await fetch(soaringForecastURL)).text();
+  const soaringForecastText = parsePreText(soaringForecastPageContent);
+  sounding(soundingData, soaringForecastText);
+})();
+
+// Async IIFE
+// Get Area Forecast data
+(async () => {
+  const areaForecastURL = 'https://forecast.weather.gov/product.php?site=NWS&issuedby=SLC&product=AFD&format=TXT&version=1';
+  const areaForecastPageContent = await (await fetch(areaForecastURL)).text();
+  const areaForecastPreText = parsePreText(areaForecastPageContent);
+  areaForecast(areaForecastPreText);
+})();
+
+// Async IIFE
+// Get Wind Map Metadata
+(async () => {
   const windMapDataURL = 'https://storage.googleapis.com/storage/v1/b/wasatch-wind-static/o/wind-map-save.png'
-  const nwsForecastURL = 'https://api.weather.gov/gridpoints/SLC/97,175/forecast'
-  const areaForecastURL = 'https://forecast.weather.gov/product.php?site=NWS&issuedby=SLC&product=AFD&format=TXT&version=1&glossary=0'
-  
-  // Open Meteo
-  const openmeteoData = await (await fetch(openmeteoURL)).json()
-  sunset = openmeteoData.daily.sunset[0]
-  hiTemp = Math.round(openmeteoData.daily.temperature_2m_max[0])
-  navSet()
-
-  // Wind Aloft
-  const gcpWindAloftData = await (await fetch(windAloftURL)).json()
-  windAloft(openmeteoData.hourly, gcpWindAloftData)
-
-  // Sounding & Soaring
-  soundingData = await (await fetch(soundingURL)).json()
-  const soaringForecastPageContent = await (await fetch(soaringForecastURL)).text()
-  const soaringForecastText = parsePreText(soaringForecastPageContent)
-  sounding(soundingData, soaringForecastText)
-
-  // Time Series
-  const timeSeriesData = await (await fetch(timeSeriesURL)).json()
-  timeSeries(timeSeriesData)
-
-  // Wind Map Metadata
   const windMapData = await (await fetch(windMapDataURL)).json()
   windMap(windMapData)
+})();
 
-  // NWS
-  const nwsForecastData = await (await fetch(nwsForecastURL)).json()
-  nwsForecast(nwsForecastData)
-
-  // Area Forecast
-  const areaForecastPageContent = await (await fetch(areaForecastURL)).text()
-  const areaForecastPreText = parsePreText(areaForecastPageContent)
-  areaForecast(areaForecastPreText)
-
-  displayImages()
-
-  // Helper function to process text for Area Forecast and Soaring Forecast:
-  function parsePreText(rawContent) {
-    const parser = new DOMParser()
-    const response = parser.parseFromString(rawContent, 'text/html')
-    const preElement = response.querySelector('pre')
-    return preElement.textContent
-  }
+// Async IIFE
+// Get NWS API data (documentation: https://www.weather.gov/documentation/services-web-api)
+(async () => {
+  const nwsForecastURL = 'https://api.weather.gov/gridpoints/SLC/97,175/forecast';
+  const nwsForecastData = await (await fetch(nwsForecastURL)).json();
+  nwsForecast(nwsForecastData);
 })();
