02e9acd2bac7bf090de3b05b89d18b98b8153aa8
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2025-08-28 08:54:13 -0600

    Update main.js

--- stage/prod/js/main.js
+++ stage/prod/js/main.js
@@ -1,9 +1,10 @@
 'use strict';
+// Global variables
 const now = new Date()
 const ftPerMeter = 3.28084
 const slider = buildNavSlider()
-const stations = ['UTOLY', 'REY', 'AMB', 'HDP', 'KSVR', 'FPS', 'OGP', 'KSLC']
-let activeNav = 0, navItems = [], sunset, soundingData
+const stations = ['UTOLY', 'REY', 'AMB', 'HDP', 'KSVR', 'FPS', 'OGP', 'KSLC']; //avoid duplicate tracking - use json coming from synoptic async?
+let activeNav = 0, navItems = [], sunset, soundingData, hiTemp = null;
 
 function reload() {
   history.scrollRestoration = 'manual'
@@ -145,13 +146,6 @@ function navSet() {
   navUpdate(activeNav)
 };
 
-function buildStationURL(stationString = '') {
-  stations.forEach(station => {
-    stationString += `&stid=${station}`
-  })
-  return stationString
-};
-
 function windMap(data) {
   const timestamp = new Date(data.timeCreated).toLocaleString('en-US', {hour: 'numeric', minute: '2-digit'}).toLowerCase();
   document.getElementById('wind-map-timestamp').innerHTML = `Wind Map @ ${timestamp}`
@@ -166,8 +160,8 @@ function extractText(text, startPattern, endPattern, offset) {
 
 function areaForecast(text) {
   const forecastDate = extractText(text, /\d{3,4}\s[PpAa][Mm]\s[Mm][DdSs][Tt]/, /\s202\d{1}\n/, 0)
-  const synopsis = extractText(text, /[Ss][Yy][Nn][Oo][Pp][Ss][Ii][Ss]/, /&&/, 0)
-  const aviation = extractText(text, /[Aa][Vv][Ii][Aa][Tt][Ii][Oo][Nn]/, /REST|.+REST\s|.+Rest\s/, 0)
+  const synopsis = extractText(text, /[Ss][Yy][Nn][Oo][Pp][Ss][Ii][Ss]/, /&&/, 8)
+  const aviation = extractText(text, /[Aa][Vv][Ii][Aa][Tt][Ii][Oo][Nn]/, /REST|.+REST\s|.+Rest\s/, 8)
   document.getElementById('area-forecast-time').innerText = forecastDate
   document.getElementById('area-forecast-synopsis').innerText = synopsis
   document.getElementById('area-forecast-aviation').innerText = aviation
@@ -197,4 +191,3 @@ function displayImages() {
   document.getElementById('cam-west').src = 'https://cameraftpapi.drivehq.com/api/Camera/GetLastCameraImage.aspx?parentID=347695945&shareID=17138700'
   document.getElementById('cam-east').src = 'https://cameraftpapi.drivehq.com/api/Camera/GetLastCameraImage.aspx?parentID=347464441&shareID=17137573'
 };
-
