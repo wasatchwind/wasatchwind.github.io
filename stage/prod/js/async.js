c4b7ca4da2a4b435db99ab50250af7e8118d2ddc
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2026-01-15 20:33:03 -0700

    Update async.js

--- stage/prod/js/async.js
+++ stage/prod/js/async.js
@@ -1,7 +1,6 @@
 "use strict";
 
-// const data = await fetchData();
-console.log(data)
+const data = await fetchData();
 main(data);
 
 async function fetchData() {
@@ -16,7 +15,7 @@ async function fetchData() {
     }
     return `${baseUrl}${query.toString()}`;
   }
-  // 600, 700, 725, 750, 775, 800, 825, 850, 875
+  
   const openMeteoParams = {
     latitude: 40.77069,
     longitude: -111.96503,
@@ -93,70 +92,5 @@ async function fetchData() {
     if (result.status === "fulfilled") data[result.value.name] = result.value.data;
     else console.error(result.response);
   }
-
   return data;
 }
-
-// TESTING
-
-function buildApiUrl(baseUrl, params, repeatKeys = []) {
-  const query = new URLSearchParams();
-  for (const [key, value] of Object.entries(params)) {
-    if (repeatKeys.includes(key) && Array.isArray(value)) value.forEach(v => query.append(key, v));
-    else query.set(key, Array.isArray(value) ? value.join(",") : value);
-  }
-  return `${baseUrl}${query.toString()}`;
-}
-
-const openMeteoParams = {
-  latitude: 40.77069,
-  longitude: -111.96503,
-  hourly: [
-    "geopotential_height_875hPa",
-    "geopotential_height_850hPa",
-    "geopotential_height_825hPa",
-    "geopotential_height_800hPa",
-    "geopotential_height_775hPa",
-    "geopotential_height_750hPa",
-    "geopotential_height_725hPa",
-    "geopotential_height_700hPa",
-    "geopotential_height_675hPa",
-    "geopotential_height_650hPa",
-    "geopotential_height_625hPa",
-    "geopotential_height_600hPa",
-    "geopotential_height_575hPa",
-    "geopotential_height_550hPa",
-    "geopotential_height_525hPa",
-    "geopotential_height_500hPa",
-    "geopotential_height_475hPa",
-    "temperature_875hPa",
-    "temperature_850hPa",
-    "temperature_825hPa",
-    "temperature_800hPa",
-    "temperature_775hPa",
-    "temperature_750hPa",
-    "temperature_725hPa",
-    "temperature_700hPa",
-    "temperature_675hPa",
-    "temperature_650hPa",
-    "temperature_625hPa",
-    "temperature_600hPa",
-    "temperature_575hPa",
-    "temperature_550hPa",
-    "temperature_525hPa",
-    "temperature_500hPa",
-    "temperature_475hPa"
-  ],
-  windspeed_unit: "mph",
-  temperature_unit: "fahrenheit",
-  forecast_hours: 12,
-  forecast_days: 1,
-  timezone: "America/Denver"
-};
-
-const url = buildApiUrl("https://api.open-meteo.com/v1/gfs?", openMeteoParams);
-
-// const response = 
-
-// const response = await(await fetch(url)).json();
-// console.log(response)
