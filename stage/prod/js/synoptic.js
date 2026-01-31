da28c218abb1381e5f6fa2f64b0c0f79f8d13643
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2026-01-30 20:19:14 -0700

    Update synoptic.js

--- stage/prod/js/synoptic.js
+++ stage/prod/js/synoptic.js
@@ -22,7 +22,7 @@ function processSynoptic(data) {
         <div class="col-6 d-flex justify-content-between me-2">
           <div class="align-self-end display-6 fw-semibold text-secondary" id="${station.STID}-time-${readingCount}">No Data</div>
           <div class="col-2 display-2" id="${station.STID}-wdir-${readingCount}"></div>
-          <div class="col-2 display-4 fw-semibold px-4 rounded-4" id="${station.STID}-wspd-${readingCount}"></div>
+          <div class="col-2 display-4 fw-semibold px-4 rounded-4 text-center" id="${station.STID}-wspd-${readingCount}"></div>
           <div class="col-2 display-6 fw-semibold gust-color" id="${station.STID}-gust-${readingCount}"></div>
         </div>
       </div>
@@ -215,3 +215,4 @@ function getZone(alti, temp, trendChar) {
   document.getElementById("zone").style.color = zone.col;
 
 }
+
