538e2ebb5a29e4fd91f63c8a2a45ac264ab20013
Matt Hanson <71908900+wasatchwind@users.noreply.github.com>
2026-02-12 08:58:49 -0700

    Update nws-api.js

--- prod/js/nws-api.js
+++ prod/js/nws-api.js
@@ -4,7 +4,7 @@
 // Soaring Forecast //
 //////////////////////
 function processSoaringForecastPage(text) {
-  if (text.search(/MINUS/)) return soaringForecastWinterFormat(text);
+  if (text.search(/MINUS/) > 0) return soaringForecastWinterFormat(text);
 
   const forecastDate = text.match(/^(.*\b\d{3,4}(?:\sAM|PM)\b.*)$/m)?.[1]?.trim();
   const rateOfLift = text.match(/Maximum rate of lift.*?(\d{1,4}\s*ft\/min.*)$/m)?.[1]?.trim();
@@ -110,4 +110,5 @@ function processGeneralForecast(data) {
   }
 
 }
+
 
